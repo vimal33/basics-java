54788c3d8eb803a64d055c90c4803f2979add119
Vimal Raj .C <118258607+vimal33@users.noreply.github.com>
2022-12-05 21:39:23 +0530

    Add files via upload

---
+++ src/basic/java/BasicJava.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basic.java;
+
+/**
+ *
+ * @author Vimal
+ */
+public class BasicJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         int number1=30;
+        int number2=60;
+        int sum=number1+number2;
+        if(sum<30)
+            
+        {
+            System.out.println("the sum is greater than 30");
+        }
+        // TODO code application logic here
+    }
+    
+}
